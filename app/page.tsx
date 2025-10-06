61ae9c68721ea76ed21a62b881c98f1367d078fe
dchu0307 <danielchu319@gmail.com>
2025-10-05 20:15:02 -0700

    adding card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -16,7 +17,15 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
+        
+        <Card className="w-full h-full">
+          <CardHeader>
+            <CardTitle>test test</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
